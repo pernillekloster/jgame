3da242e9b2de114241738b96301df93aa35ad617
Pernille Kloster <pernillekloster@Pernilles-MacBook-Pro.local>
2018-10-17 17:03:20 +0200

    Add of instructions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log("hello");
+
+//Iteration 1
+function goToPage(link) {
+  //goal: hide all the divs (jQuery) with data-page, except the right one
+}
+//Go to main page
+
+goToPage("play");
+
+// Iteration 2: listen for click events on <a>, you will need to use event.preventDefault
